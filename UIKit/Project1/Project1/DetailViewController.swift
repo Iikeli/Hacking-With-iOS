8886b3a5d9a42deb1b20bded92c0c07cf4f4dff3
Iiro Alhonen <iiro.alhonen@gmail.com>
2020-07-30 14:30:21 +0300

    Working with Project 1 with UIKit.

---
+++ UIKit/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Iiro Alhonen on 30.7.2020.
+//  Copyright © 2020 Iiro Alhonen. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
