4b545c245b8522c7d33e9e4e3171e8b6a10dd4d7
Iiro Alhonen <iiro.alhonen@gmail.com>
2020-07-31 15:32:42 +0300

    Initial setup for Project 2.

---
+++ UIKit/Project2/Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Iiro Alhonen on 31.7.2020.
+//  Copyright © 2020 Iiro Alhonen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
